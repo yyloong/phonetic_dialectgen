2d03197535639d56d3b13ab82f6e4fded6077616
flowermouse <huashulizi@gmail.com>
2025-07-15 12:16:53 +0800

    fix conflict

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from tokenizer import TTSTokenizer
-
-csv_file = "data_with_pinyinIPA.csv"
-file = pd.read_csv(csv_file)
-tokenizer = TTSTokenizer()
-for i in range(len(file)):
-    print(file["audio"][i])
-    token_ids = tokenizer(file["IPA"][i])
