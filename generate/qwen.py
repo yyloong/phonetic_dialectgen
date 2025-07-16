9d7a8baf4a2d21e4a56b7e3be5ef1c61ab8081a2
flowermouse <huashulizi@gmail.com>
2025-07-16 11:35:58 +0800

    add comment

--- generate/qwen.py
+++ generate/qwen.py
@@ -6,6 +6,8 @@ import dashscope
 from dotenv import load_dotenv
 
 # Ensure you have the DASHSCOPE_API_KEY set in your environment variables
+# 在当前目录下创建一个 .env 文件，并添加以下内容：
+# DASHSCOPE_API_KEY=your_api_key_here
 load_dotenv()
 
 text = "那我来给大家推荐一款T恤，这款呢真的是超级好看，这个颜色呢很显气质，而且呢也是搭配的绝佳单品，大家可以闭眼入，真的是非常好看，对身材的包容性也很好，不管啥身材的宝宝呢，穿上去都是很好看的。推荐宝宝们下单哦。"
