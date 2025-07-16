64217637c682e2689e076cb667f063a3997cd80e
flowermouse <huashulizi@gmail.com>
2025-07-16 08:18:01 +0800

    add edge tts

---
+++ edge.py
@@ -0,0 +1,15 @@
+import subprocess
+import pandas as pd
+import tqdm
+import time
+
+file = pd.read_csv("cv-hk.csv")
+
+for i in tqdm.tqdm(range(len(file))):
+    text = file.iloc[i]["text"]
+    command = f"edge-tts --voice zh-HK-HiuMaanNeural --text '{text}' --write-media edge/{i+1}.wav"
+    subprocess.run(command, shell=True)
+    file.loc[i, "wav"] = f"{i+1}"
+    time.sleep(0.1)
+
+file.to_csv("edge-hk.csv", index=False)
