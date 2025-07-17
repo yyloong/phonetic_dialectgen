3f051c06a20183dd78e10fc5d5c66792ed90870e
flowermouse <huashulizi@gmail.com>
2025-07-17 10:11:47 +0800

    clean data

--- generate/index.py
+++ generate/index.py
@@ -6,7 +6,7 @@ import csv
 tts = IndexTTS(model_dir="/home/u-wuhc/index-tts/checkpoints",cfg_path="/home/u-wuhc/index-tts/checkpoints/config.yaml")
 voice = "/home/u-wuhc/index-tts/audio/sbn.wav"
 
-tts.infer(voice, "暑假来了，你感受到预期的放松和快乐了吗？", "output.wav")
+tts.infer(voice, "我真是一个天才！", "output.wav")
 
 # with open('data.csv', 'r') as file:
 #     reader = csv.reader(file)
