e7942c3dd5ee12cd934cb2799d1356b0f8fa4e61
flowermouse <huashulizi@gmail.com>
2025-07-18 15:18:38 +0800

    clean

--- shuyu/clean0.py
+++ shuyu/clean0.py
@@ -14,28 +14,4 @@ with open('shupin.dict.yaml', 'r', encoding='utf-8') as file, open(output, 'w', 
         if value[0] >= 'A' and value[0] <= 'Z':
             continue
         out.write(f'{line[0]}: {value}\n')
-
-# import yaml
-
-# with open('dict_cleaned.yaml', 'r', encoding='utf-8') as file:
-#     data = yaml.safe_load(file)
-
-# # 繁体字转换为简体字
-
-# print(data.keys())
-
-# from opencc import OpenCC
-
-# cc = OpenCC('t2s')
-
-# with open('dict_simp.yaml', 'w', encoding='utf-8') as out:
-#     for i in range(len(data)):
-        
-
-# with open('7000.txt', 'r', encoding='utf-8') as file:
-#     with open('7000_simp.txt', 'w', encoding='utf-8') as out:
-#         for line in file:
-#             line = line.strip()
-#             for char in line:
-#                 out.write(f'{char}\n')
         
