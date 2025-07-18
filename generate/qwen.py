4578bd921104fca5fc45f5745eb39a18cf4fa005
flowermouse <huashulizi@gmail.com>
2025-07-18 16:21:56 +0800

    add qwen generate data

--- generate/qwen.py
+++ generate/qwen.py
@@ -10,12 +10,12 @@ from dotenv import load_dotenv
 # DASHSCOPE_API_KEY=your_api_key_here
 load_dotenv()
 
-text = "那我来给大家推荐一款T恤，这款呢真的是超级好看，这个颜色呢很显气质，而且呢也是搭配的绝佳单品，大家可以闭眼入，真的是非常好看，对身材的包容性也很好，不管啥身材的宝宝呢，穿上去都是很好看的。推荐宝宝们下单哦。"
+text = "萤火虫在回忆中跳舞，诉说着古老的故事。"
 response = dashscope.audio.qwen_tts.SpeechSynthesizer.call(
     model="qwen-tts-latest",
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     text=text,
-    voice="Dylan",
+    voice="Sunny",
 )
 # voice: 
 # Dylan=北京话
---
+++ generate/sunny.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+import requests
+import tqdm
+# pip install dashscope
+import dashscope
+# pip install python-dotenv
+from dotenv import load_dotenv
+
+# Ensure you have the DASHSCOPE_API_KEY set in your environment variables
+# 在当前目录下创建一个 .env 文件，并添加以下内容：
+# DASHSCOPE_API_KEY=your_api_key_here
+load_dotenv()
+
+df = pd.read_csv("aitts3_shu.csv", encoding='utf-8')
+
+for i in tqdm.tqdm(range(len(df))):
+    text = df.loc[i, 'text']
+    response = dashscope.audio.qwen_tts.SpeechSynthesizer.call(
+        model="qwen-tts-latest",
+        api_key=os.getenv("DASHSCOPE_API_KEY"),
+        text=text,
+        voice="Sunny", 
+    )
+    audio_url = response.output.audio["url"]
+    save_path = f"sichuan/{i+1}.wav"  # 自定义保存路径
+
+    response = requests.get(audio_url)
+    response.raise_for_status()  # 检查请求是否成功
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+    print(f"音频文件已保存至：{save_path}")
