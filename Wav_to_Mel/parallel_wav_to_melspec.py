445753c61ade5869e5520ceeda8c6359b7ad9cc0
yyloong <2516606902@qq.com>
2025-07-27 09:36:30 +0800

    fix wav_to_mel

--- Wav_to_Mel/parallel_wav_to_melspec.py
+++ Wav_to_Mel/parallel_wav_to_melspec.py
@@ -4,28 +4,11 @@ import sys
 import multiprocessing as mp
 import os
 import torch
-import librosa
+from wav_to_melspec import get_spectrogram
 
 
-def get_spectrogram(path, turn):
-    '''TODO:change the parameter to fit the model if you use another moder'''
-    signal, _ = librosa.load(path, sr=22050, mono=True)
-    device = f"cuda:{turn%4}"
-    signal = torch.Tensor(signal).to(device)
-    spectrogram, _ = mel_spectrogram(
-        y=signal.squeeze(),
-        n_fft=1024,
-        n_mels=80,
-        sample_rate=22050,
-        hop_size=256,
-        win_size=1024,
-        fmin=0,
-        fmax=None,
-    )
-    return spectrogram
 
-
-def parallel_process(wav_dir, save_dir, process_fun, num_workers=mp.cpu_count()):
+def parallel_process(wav_dir, save_dir, process_fun, num_workers=int(mp.cpu_count()/2)):
     '''.wav file to .pt file,wav_dir is the dir of input files the output will be save in save_dir'''
     os.makedirs(save_dir, exist_ok=True)
     listdir = os.listdir(wav_dir)
@@ -41,7 +24,7 @@ def parallel_process(wav_dir, save_dir, process_fun, num_workers=mp.cpu_count())
             tqdm(
                 pool.starmap(
                     process_fun,
-                    zip(wav_paths, save_paths, list(range(len(wav_paths)))),
+                    zip(wav_paths, save_paths),
                 ),
                 total=len(wav_paths),
                 desc="Processing WAV files",
@@ -52,11 +35,11 @@ def parallel_process(wav_dir, save_dir, process_fun, num_workers=mp.cpu_count())
     return results
 
 
-def process_file(wav_path, save_path, turn):
+def process_file(wav_path, save_path):
     if os.path.exists(save_path):
         return save_path
     try:
-        mel_spec = get_spectrogram(wav_path, turn)
+        mel_spec = get_spectrogram(wav_path)
         torch.save(mel_spec, save_path)
     except:
         print(f"{os.path.basename(save_path)}", end=",")
@@ -65,6 +48,6 @@ def process_file(wav_path, save_path, turn):
 
 if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
-    wave_path = "your path"
-    save_path = "your path"
+    wave_path = "/home/u-longyy/week2/Qwen-tts_new"
+    save_path = "/home/u-longyy/week2/Qwen-tts_new_mel"
     parallel_process(wave_path, save_path, process_file)
